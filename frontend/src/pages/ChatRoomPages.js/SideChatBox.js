f9a06f0d509dab1730bfae3ebdb9b53eef11ab3f
Akash <akashsingh2210670@gmail.com>
2024-01-24 22:36:01 +0530

    Fixed Click error
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- frontend/src/pages/ChatRoomPages.js/SideChatBox.js
+++ frontend/src/pages/ChatRoomPages.js/SideChatBox.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { FaUserGroup } from "react-icons/fa6";
 
-function SideChatBox(props) {
+function SideChatBox({ subject, lastmessage, time, onClick }) {
   return (
-    <div className='flex items-center'>
+    <div className='cursor-pointer' onClick={onClick}>
         <img className='h-14 w-14' src="https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg" alt="" />
         <div className='flex flex-col justify-between flex-1'>
-            <p>{props.subject}</p>
+            <p>{subject}</p>
             <div className='flex'>
-                <p className='flex-1'>{props.lastmessage}</p>
-                <p className='mx-2'>{props.time}</p>
+                <p className='flex-1'>{lastmessage}</p>
+                <p className='mx-2'>{time}</p>
             </div>
         </div>
     </div>
