948700bbb53899e8c25c54eee445ca3447d15f6a
Akash <akashsingh2210670@gmail.com>
2024-01-24 23:58:59 +0530

    Fixed API request
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- frontend/src/pages/ChatRoomPages.js/SideChat.js
+++ frontend/src/pages/ChatRoomPages.js/SideChat.js
@@ -48,7 +48,7 @@ function SideChat({onChatClick}) {
       console.log("This is the selected contact")
       console.log(selectedContact)
       console.log("And this is what we are sending")
-      console.log(JSON.stringify({"email": "john_doe", "receiver": selectedContact[0]}))
+      console.log(dummyUser)
       
 
       // Fetch messages for the selected user and receiver
@@ -57,10 +57,7 @@ function SideChat({onChatClick}) {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          email: "john_doe", // The logged-in user (replace with actual user data)
-          receiver: selectedContact[0], // Assuming the first element in the list is the subject (email)
-        }),
+        body: JSON.stringify(dummyUser),
       });
 
       if (response.ok) {
